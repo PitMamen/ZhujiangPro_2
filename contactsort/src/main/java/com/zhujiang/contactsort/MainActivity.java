8072f9c9e929a84f4612435bbfa7df8c6507f247
zhujiang <zhujiang.shishi@foxmail.com>
2016-08-30 16:57:12 +0800

    add letter sort list

--- contactsort/src/main/java/com/zhujiang/contactsort/MainActivity.java
+++ contactsort/src/main/java/com/zhujiang/contactsort/MainActivity.java
@@ -8,19 +8,21 @@ import android.content.Loader;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AlphabetIndexer;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,9 +39,24 @@ public class MainActivity extends AppCompatActivity
     private LinearLayout titleLayout;
 
     /**
+     * 分组式布局
+     */
+    private RelativeLayout sectionToastLayout;
+
+    /**
+     * 右侧可滑动字母表
+     */
+    private Button mBtnAlphabet;
+
+    /**
      * 分组上显示的字母
      */
     private TextView title;
+
+    /**
+     * 弹出式分组上的文字
+     */
+    private TextView sectionToastText;
 
     /**
      * 联系人列表
@@ -71,6 +88,9 @@ public class MainActivity extends AppCompatActivity
      */
     private int lastFirstVisibleItem = -1;
 
+    /**
+     * 记录当前操作的联系人游标，以便activity销毁时，关闭它
+     */
     private Cursor cursor;
 
 
@@ -81,24 +101,25 @@ public class MainActivity extends AppCompatActivity
         requestPermissions();
     }
 
+    /**
+     * 兼容android 6.0 Runtime permission请求方式
+     */
     private void requestPermissions() {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int state = checkSelfPermission(Manifest.permission.READ_CONTACTS);
-            if (state != PackageManager.PERMISSION_GRANTED) {
-                // 如果没有权限
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this
-                        , Manifest.permission.READ_CONTACTS)) {
-                    Toast.makeText(this, "Give this app contact permisson!"
-                            , Toast.LENGTH_SHORT).show();
-                } else {
-                    // 请求申请权限
-                    ActivityCompat.requestPermissions(this
-                            , new String[]{Manifest.permission.READ_CONTACTS}, 0);
-                }
+        int state = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (state != PackageManager.PERMISSION_GRANTED) {
+            // 如果没有权限
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this
+                    , Manifest.permission.READ_CONTACTS)) {
+                Toast.makeText(this, "Give this app contact permisson!"
+                        , Toast.LENGTH_SHORT).show();
             } else {
-                initialization();
+                // 请求申请权限
+                ActivityCompat.requestPermissions(this
+                        , new String[]{Manifest.permission.READ_CONTACTS}, 0);
             }
+        } else {
+            // 如果已经获取该权限了
+            initialization();
         }
     }
 
@@ -118,7 +139,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onDestroy() {
-        if (!cursor.isClosed()) {
+        if (cursor != null && !cursor.isClosed()) {
             cursor.close();
             Log.i("TAG", "cursor closed!");
         }
@@ -129,27 +150,24 @@ public class MainActivity extends AppCompatActivity
         titleLayout = (LinearLayout) findViewById(R.id.title_layout);
         title = (TextView) findViewById(R.id.title1);
         mContactListView = (ListView) findViewById(R.id.contacts_list_view);
+        mBtnAlphabet = (Button) findViewById(R.id.alphabetButton);
+        sectionToastLayout = (RelativeLayout) findViewById(R.id.section_toast_layout);
+        sectionToastText = (TextView) findViewById(R.id.section_toast_text);
         mAdapter = new ContactAdapter(this, R.layout.contact_item, mContacts);
 
         /**
          * 由于startManagingCursor(Cursor c)方法失效，因为此方法的数据库操作在UI线程中
-         * 如果有大量数据会导致阻塞
+         * 如果有大量数据会导致线程阻塞
          *
          * 使用LoaderManager来实现一个LoaderCallbacks接口，创建一个类继承CursorLoader类
          * 在onLoadInBackground方法中查询数据库，
          * 在LoaderCallbacks的onCreateLoader方法返回CursorLoader实例
          *
          */
+
         getLoaderManager().initLoader(1, null, this);
     }
 
-    /**
-     * 设置联系人ListView监听事件,根据当前状态进行分组显示，从而实现挤压动画效果
-     */
-    private void setupContactListView() {
-        mContactListView.setAdapter(mAdapter);
-        mContactListView.setOnScrollListener(this);
-    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -171,9 +189,10 @@ public class MainActivity extends AppCompatActivity
         mIndexer = new AlphabetIndexer(cursor, 1, alphabet);
         mAdapter.setIndexer(mIndexer);
         if (mContacts.size() > 0) {
-            setupContactListView();
+            mContactListView.setAdapter(mAdapter);
+            mContactListView.setOnScrollListener(this);
+            setAlphabetListener();
         }
-
         this.cursor = cursor;
     }
 
@@ -270,7 +289,7 @@ public class MainActivity extends AppCompatActivity
             View childView = view.getChildAt(0);
             if (childView != null) {
                 int titleHeight = titleLayout.getHeight();
-                int bottom = view.getBottom();
+                int bottom = childView.getBottom();
                 ViewGroup.MarginLayoutParams params =
                         (ViewGroup.MarginLayoutParams) titleLayout.getLayoutParams();
                 // 如果一个item的高度小于titleLayout的高度,就开始挤压titleLayout
@@ -278,6 +297,11 @@ public class MainActivity extends AppCompatActivity
                     float pushedDistance = bottom - titleHeight;
                     params.topMargin = (int) pushedDistance;
                     titleLayout.setLayoutParams(params);
+                } else {
+                    if (params.topMargin != 0) {
+                        params.topMargin = 0;
+                        titleLayout.setLayoutParams(params);
+                    }
                 }
             }
         }
@@ -286,6 +310,55 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    /**
+     * 设置字母表上的触摸事件，根据当前触摸的位置结合字母表的高度，计算出当前触摸在哪个字母表上
+     * 当手指触摸在字母表上时，展示弹出式分组。手指离开字母表时，将弹出式分组隐藏。
+     */
+    private void setAlphabetListener() {
+
+        mBtnAlphabet.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float alphabetHeight = mBtnAlphabet.getHeight();
+                float y = event.getY();
+                int sectionPosition = (int) ((y / alphabetHeight) / (1f / 27f));
+                if (sectionPosition < 0) {
+                    sectionPosition = 0;
+                } else if (sectionPosition > 26) {
+                    sectionPosition = 26;
+                }
+
+//                String sectionLetter = String.valueOf(alphabet.charAt(sectionPosition));
+                int position = mIndexer.getPositionForSection(sectionPosition);
+                String sectionLetter = mContacts.get(position).getSortKey();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mBtnAlphabet.setBackgroundResource(R.drawable.a_z_click);
+                        sectionToastLayout.setVisibility(View.VISIBLE);
+                        sectionToastText.setText(sectionLetter);
+                        mContactListView.setSelection(position);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        sectionToastText.setText(sectionLetter);
+                        mContactListView.setSelection(position);
+                        break;
+                    default:
+                        mBtnAlphabet.setBackgroundResource(R.drawable.a_z);
+                        sectionToastLayout.setVisibility(View.GONE);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+
+    /**
+     * 设置一个load cursor的类
+     */
     static class MyCurorLoader extends CursorLoader {
 
         public MyCurorLoader(Context context) {
